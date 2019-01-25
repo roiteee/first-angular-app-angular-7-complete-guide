ea875a2352d7106159337a47640efc867d31f81a
rohit_thapa <stylewatchmagazine@gmail.com>
2019-01-25 10:58:53 +0545

    upto lessons 14 Property binding, basics, now shows button that is disabled but is enabled after 2 seconds

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus: string = 'offliiiine';
+
+  getServerStatus(): string {
+    return this.serverStatus;
+  }
+}
